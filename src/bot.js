2fd0385007ae1074db23f0c4414aea31a35874c4
watatomo <colettesparkle@gmail.com>
2020-12-24 16:13:48 -0800

    New lines (Dead End Street 6-7)

--- src/bot.js
+++ src/bot.js
@@ -5,9 +5,6 @@ const bot = new Twit(config)
 
 console.log("Bot has started!")
 
-var prevQuoteIndex = -1; // Initialize outside of function
-
-
 function postRandomQuote() {
   // Pick a random quote
   var quote = quotes[Math.floor(Math.random()*quotes.length)]
@@ -62,91 +59,6 @@ function postQuote(quote) {
   }
 }
 
-// Try to fix stuff below
-
-function replyRandomQuote() {
-  var stream = bot.stream('statuses/filter', { track: '@wttm_qtbot random'})
-
-  stream.on('tweet', function (tweet) {
-    var reply = '@'
-    reply += tweet.user.screen_name
-    reply += postRandomQuote();
-
-    console.log(reply)
-
-    try {
-      bot.post('statuses/update', { status: quote }, function(err, data, response) {
-        console.log(data)
-      });
-    } catch (error) {
-      console.log(error)
-    }
-  })
-}
-
-function getRepliesAskingForSource(callback) {
-  bot.get('search/tweets', { q: 'to:@wttm_qtbot source', count: 100 }, callback)
-}
-
-function getRepliesByBot(tweet, callback) {
-  var since_id = tweet.id_str
-  bot.get('search/tweets', { q: 'from:@wttm_qtbot to:' + tweet.user.screen_name, since_id: since_id, count: 10 }, callback)
-}
-
-function replyAllWithSource() {
-  getRepliesAskingForSource(function(err, data, response) {
-    if (err) {
-      console.log(err)
-    } else {
-      data.statuses.forEach(s => {
-        getRepliesByBot(s, function(err, data, response) {
-          if (data.statuses.length == 0) {
-            replyWithSource(s);
-          }
-        });
-      })
-    }
-  })
-}
-
-function replyWithSource(tweet) {
-  getParentTweet(tweet, function(err, data, response) {
-    console.log("--")
-    console.log(tweet.text)
-    var reply = '@'
-    reply += tweet.user.screen_name
-    reply += ' This quote is from '
-    reply += quotes.character
-    reply += ' in '
-    reply += quotes.story
-    reply += '.'
-
-    console.log(reply)
-
-    if (config.post_to_twitter) {
-      bot.post('statuses/update', { status: reply, in_reply_to_status_id: tweet.id_str }, function(err, data, response) {
-        if (err) {
-          console.log(err)
-        } else {
-          console.log(data)
-        }
-      })
-    } else {
-      console.log("Replying to user failed. ENV variable POST_TO_TWITTER has to be set to true.")
-    }
-  })
-}
-
-function getParentTweet(tweet, callback) {
-  bot.get('statuses/show/:id', { id: tweet.in_reply_to_status_id_str }, callback)
-}
-
 module.exports.shortenQuote = shortenQuote;
 module.exports.quotes = quotes;
-module.exports.getRepliesAskingForSource = getRepliesAskingForSource;
-module.exports.replyWithSource = replyWithSource;
-module.exports.replyAllWithSource = replyAllWithSource;
-module.exports.getParentTweet = getParentTweet;
-module.exports.getRepliesByBot = getRepliesByBot;
 module.exports.postRandomQuote = postRandomQuote;
-module.exports.replyRandomQuote = replyRandomQuote;
